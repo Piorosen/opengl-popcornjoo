7919c5be7cb34b80cfad430ee07229f5fdca6a43
Piorosen <aoikazto@gmail.com>
2022-05-24 01:48:34 +0900

    팝콘쥬우

--- screen_block_break/convert.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-#include "data_structure.h"
-
-namespace graphics {
-	namespace convert {
-		graphics::point screenToGraphic(const graphics::point&);
-	}
-}
--- screen_block_break/main.cpp
+++
@@ -1,9 +0,0 @@
-#include "application.h"
-
-int main(int argc, char** argv)
-{
-    graphics::initDisplay(&argc, argv,
-        graphics::size{ 400, 400 },
-        "재미난 게임!"
-    );
-}
