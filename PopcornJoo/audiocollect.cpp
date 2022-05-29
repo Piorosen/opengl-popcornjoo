d6baa57211e574280af18e0304927c24d814d10e
Piorosen <aoikazto@gmail.com>
2022-05-29 13:26:23 +0900

    FMOD를 통한 오디오 엔진 구현함.

--- PopcornJoo/audiocollect.cpp
+++ PopcornJoo/audiocollect.cpp
@@ -1,1 +1,29 @@
 #include "audiocollect.h"
+
+std::optional<FMOD::Sound*> grc::audiocollect::get(std::string file)
+{
+    if (audioId.find(file) != audioId.end())
+    {
+        return audioId[file];
+    }
+    else
+    {
+        return std::nullopt;
+    }
+}
+
+grc::audioresult grc::audiocollect::add(std::string file, audiomode mode)
+{
+    FMOD::Sound* sound;
+    auto result = this->pSystem->createSound(file.c_str(), (int)mode, NULL, &sound);
+    audioId[file] = sound;
+
+    return (grc::audioresult)result;
+}
+
+grc::audioresult grc::audiocollect::play(std::string audio, FMOD::Channel** output)
+{
+    auto result = pSystem->playSound(audioId[audio], NULL, 0, &output);
+    return (grc::audioresult)result;
+}
+
--- PopcornJoo/audiocollect.h
+++ PopcornJoo/audiocollect.h
@@ -2,3 +2,45 @@
 #include <fmod/fmod.hpp>
 #include <fmod/fmod_errors.h>
 
+#include <string>
+#include <map>
+#include <vector>
+#include <optional>
+#include <memory>
+#include <spdlog/spdlog.h>
+
+#include "audiomode.h"
+#include "audioresult.h"
+
+namespace grc
+{
+    class audiocollect
+    {
+    private:
+        std::map<std::string, FMOD::Sound*> audioId;
+        FMOD::System* pSystem;
+
+    public:
+        audiocollect() {
+            auto d = FMOD::System_Create(&pSystem);
+            if (d != FMOD_OK) {
+                spdlog::error("audio driver fail!");
+            }
+            else {
+                spdlog::info("audio driver success!");
+            }
+        }
+
+        static std::unique_ptr<grc::audiocollect> shared;
+
+        std::optional<FMOD::Sound*> get(std::string file);
+        audioresult add(std::string file, audiomode mode = audiomode::DEFAULT);
+        audioresult play(std::string audio, FMOD::Channel** output);
+
+
+    protected:
+    };
+}
+
+
+std::unique_ptr<grc::audiocollect> grc::audiocollect::shared = std::make_unique<grc::audiocollect>();
