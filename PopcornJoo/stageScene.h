5328bb2b70e40f15ce14cd4fdbe6b7ac849fab85
Piorosen <aoikazto@naver.com>
2022-06-01 21:17:55 +0900

    스테이지 선택을위한 리소스 및 적용함.

--- PopcornJoo/stageScene.h
+++ PopcornJoo/stageScene.h
@@ -17,12 +17,22 @@ std::shared_ptr<grc::scene> getStageScene(std::function<void()> close, std::func
 		spdlog::info("file find [{}]", file.path().filename().stem().string());
 		files.push_back(file.path().filename().stem().string());
 	}
+	
+	int stageSelect = grc::imagecollect::shared->add(".\\resources\\imaegs\\game\\stageselect.png");
+	auto background = std::make_shared<grc::view>(grc::rect(0, 0, 1280, 800), stageSelect);
 
 	std::sort(files.begin(), files.end());
-	std::vector<int> bNum;
+	std::vector<int> wNum;
 	for (int i = 0; i < 10; i++) {
-		bNum.push_back(grc::imagecollect::shared->add(".\\resources\\imaegs\\game\\blackNum\\" + std::to_string(i) + ".png"));
+		wNum.push_back(grc::imagecollect::shared->add(".\\resources\\imaegs\\game\\whiteNum\\" + std::to_string(i) + ".png"));
 	}
+
+	std::vector<int> level;
+	for (int i = 0; i < 4; i++) {
+		level.push_back(grc::imagecollect::shared->add(".\\resources\\imaegs\\game\\stageLevel\\" + std::to_string(i) + ".png"));
+	}
+
+
 	//1180
 	//400 ~ 750
 	//	350
@@ -35,17 +45,18 @@ std::shared_ptr<grc::scene> getStageScene(std::function<void()> close, std::func
 	int maxY = 4;
 	int maxX = 5;
 
+	data->view.push_back(background);
 	for (int idx = 0; idx < files.size(); idx++) {
 		int capture = idx;
 		int y = idx / maxX;
 		int x = idx % maxX;
 		int padding = 10;
-		auto c = std::make_shared<grc::colorbuttonview>(grc::rect(50 + 1180 / maxX * x + padding, 400 + (350 / maxY) * y + padding,
-			50 + (1180 / maxX) * (x + 1) - padding, 400 + (350 / maxY) * (y + 1) - padding));
+		auto c = std::make_shared<grc::buttonview>(grc::rect(50 + 1180 / maxX * x + padding, 400 + (350 / maxY) * y + padding,
+			50 + (1180 / maxX) * (x + 1) - padding, 400 + (350 / maxY) * (y + 1) - padding), level[y], level[y], level[y]);
 		auto text = std::make_shared<grc::numview>(grc::rect(50 + 1180 / maxX * x , 400 + (350 / maxY) * y + (padding * 2),
-			50 + (1180 / maxX) * (x + 1), 400 + (350 / maxY) * (y + 1) - +(padding * 2)), bNum, idx + 1);
+			50 + (1180 / maxX) * (x + 1), 400 + (350 / maxY) * (y + 1) - +(padding * 2)), wNum, idx + 1);
 		
-		c->mouseEvent = [capture, startMap](grc::colorbuttonview* self, grc::buttonstate state) {
+		c->mouseEvent = [capture, startMap](grc::buttonview* self, grc::buttonstate state) {
 			if (state == grc::buttonstate::mouseUp) {
 				spdlog::info("Start Game : [{}]", capture + 1);
 				char dataText[100];
