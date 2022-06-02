030cb3684f1a8af276ad12f0b77700901d907c42
Piorosen <aoikazto@naver.com>
2022-06-02 16:27:11 +0900

    속도를 수정함.

--- PopcornJoo/main.cpp
+++ PopcornJoo/main.cpp
@@ -4,7 +4,7 @@
 int main(int argc, char** argv) {
 	spdlog::info("Build Time : [{}, {}]", __DATE__, __TIME__);
 	grc::application::shared->initialize(grc::size{ 1280, 800 }, "차차의 모험기");
-	grc::application::shared->setRenderSpeed(10);
+	grc::application::shared->setRenderSpeed(1);
 	grc::application::shared->setScene(entryPoint());
 
 	grc::application::shared->run();
